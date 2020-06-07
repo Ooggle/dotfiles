281f7502facf3db11621f932f63fb23784e104eb
Ooggle <33269056+Ooggle@users.noreply.github.com>
2020-06-07 20:29:42 +0200

    dynamic monitors

--- root/sc/monitors.sh
+++ root/sc/monitors.sh
@@ -2,36 +2,44 @@
 
 if [ -z "$1" ]
 then
-        echo "PLEASE CHOOSE AN OPTION : dleft / dright / none "
-        echo "exiting..."
-        exit
+    echo "PLEASE CHOOSE AN OPTION : dleft / dright / none "
+    echo "exiting..."
+    exit
 fi
 
 if [ $1 = "dleft" ]
 then
-        echo "setting second screen to left screen"
-	xrandr --auto
-        xrandr --output eDP-1 --auto --right-of HDMI-1
-        #reload wallpaper
-	feh --bg-fill /root/.config/i3/background.png
-        exit
+    display1=$(xrandr --listmonitors | grep -Eo '[A-Za-z]+\-1$' -m1)
+    display2=$(xrandr --listmonitors | grep -Eo '[A-Za-z]+\-1$' -m2 | tail -n1)
+    echo "setting second screen to left screen"
+    xrandr --auto
+    xrandr --output $display1 --auto --right-of $display2
+    #reload wallpaper
+    feh --bg-fill /root/.config/i3/background.png
+    exit
 fi
 
 if [ $1 = "dright" ]
 then
-        echo "setting second screen to right screen"
-	xrandr --auto
-        xrandr --output eDP-1 --auto --left-of HDMI-1 
-        #reload wallpaper
-        feh --bg-fill /root/.config/i3/background.png
-        exit
+    display1=$(xrandr --listmonitors | grep -Eo '[A-Za-z]+\-1$' -m1)
+    display2=$(xrandr --listmonitors | grep -Eo '[A-Za-z]+\-1$' -m2 | tail -n1)
+    echo "setting second screen to right screen"
+    xrandr --auto
+    xrandr --output $display1 --auto --left-of $display2
+    #reload wallpaper
+    feh --bg-fill /root/.config/i3/background.png
+    exit
 fi
 
 if [ $1 = "none" ]
 then
-        echo "removing second screen"
-	xrandr --output HDMI-1 --off
-        #reload wallpaper
-        feh --bg-fill /root/.config/i3/background.png
-        exit
+    display2=$(xrandr --listmonitors | grep -Eo '[A-Za-z]+\-1$' -m2 | tail -n1)
+    echo "removing second screen"
+    xrandr --output $display2 --off
+    #reload wallpaper
+    feh --bg-fill /root/.config/i3/background.png
+    exit
 fi
+
+echo "PLEASE CHOOSE AN OPTION : dleft / dright / none "
+echo "exiting..."
