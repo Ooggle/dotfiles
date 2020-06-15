10670485c7da3fcdc904da60d96a6a60e7b87cba
Ooggle <33269056+Ooggle@users.noreply.github.com>
2020-06-15 07:40:34 -0500

    writed autoconfig script

--- autoconfig.sh
+++ autoconfig.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
 
+# intro
+echo "- Auto configure script -"
+echo "This script need to run with root privileges."
+echo "Please use this with a Ubuntu/Debian based distro."
+
+# install dependencies
+apt -y install i3blocks feh maim xclip pulseaudio rxvt-unicode \
+xserver-xorg-input-synaptics scrot ffmpeg imagemagick
+
+# move config files
+mv etc/ /
+mv root/ /
+mv usr/ /
+
+# install nerd fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip
+unzip DroidSansMono.zip -d ~/.fonts
+fc-cache -fv
+rm -r DroidSansMono.zip
+
 # set chrome as default x web browser
 xdg-settings set default-web-browser chrome.desktop
+
+#remove folder after finished
+CURRENTDIR=$(pwd)
+cd ..
+rm -r $CURRENTDIR
+echo "Configuration complete."
