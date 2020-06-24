18b7d52fca5986fd754f4bf8e4ae9896cebfea02
Ooggle <33269056+Ooggle@users.noreply.github.com>
2020-06-24 20:23:52 +0200

    Create wpaconnect.sh

---
+++ root/sc/wpaconnect.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+    echo "PLEASE CHOOSE A WPA CONFIG FILE "
+    echo "exiting..."
+    exit
+else
+    wpa_supplicant -D nl80211 -B -i wlp2s0 -c $1
+    dhclient wlp2s0
+    exit
+fi
